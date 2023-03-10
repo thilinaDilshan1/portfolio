2850dc566c6e86d5f4b8344418a17792b53c3f95
Thilina Dilshan <tilinadilshan1999@gmail.com>
2023-03-10 10:14:47 +0530

    ABOUT ME

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multi-text", {
+    strings: ["Frontend Developer", "Graphic Designer", "UI/UX Designer", "Programmer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
