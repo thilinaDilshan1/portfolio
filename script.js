b6bc1b9177e5b7c4a84c0be9f67d45c3120a3f82
Thilina Dilshan <tilinadilshan1999@gmail.com>
2023-03-11 11:52:50 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multi-text", {
-    strings: ["Frontend Developer", "Graphic Designer", "UI/UX Designer", "Programmer"],
+    strings: [" ", "Frontend Developer", "Graphic Designer", "UI/UX Designer", "Programmer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
